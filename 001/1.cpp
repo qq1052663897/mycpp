ba0c11280950b55f8ee75a495c6e81ffeedb87cb
zhangtengfei <1052663897@qq.com>
2021-08-31 17:33:53 +0800

    提交代码

--- 001/1.cpp
+++ 001/1.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main()
 {
-    cout << "Hello Vscode" << endl;
+    int int1, int2;
+    string str = "Hello C plus plus !";
+    cout << "input something number please" << endl;
+    cin >> int1;
+    cout << "input something number else please" << endl;
+    cin >> int2;
+    cout << "calculating...\n"
+        << int1 + int2 << endl;
+    cout << str << endl;    
     return 0;
 }
